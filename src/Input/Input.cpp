4dab1dae5f1aba964a99c5e8879bde96514b3c85
MAIPA01 <47190521+MAIPA01@users.noreply.github.com>
2024-03-11 17:00:21 +0100

    Input sample

--- src/Input/Input.cpp
+++ src/Input/Input.cpp
@@ -1,1 +1,197 @@
 #include "Input/Input.h"
+
+using namespace std;
+
+GLFWwindow* Input::_mainWindow = nullptr;
+
+map<GLFWwindow*, bool*> Input::_mouseButtonPressed = map<GLFWwindow*, bool*>();
+map<GLFWwindow*, bool*> Input::_mouseButtonHolded = map<GLFWwindow*, bool*>();
+
+map<GLFWwindow*, bool*> Input::_keyPressed = map<GLFWwindow*, bool*>();
+map<GLFWwindow*, bool*> Input::_keyHolded = map<GLFWwindow*, bool*>();
+
+void Input::key_callback(GLFWwindow* win, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_UNKNOWN) return; // Don't accept unknown keys
+	if (action == GLFW_PRESS) {
+		if (_keyPressed[win][key]) {
+			_keyHolded[win][key] = true;
+		}
+		else {
+			_keyPressed[win][key] = true;
+		}
+	}
+	else if (action == GLFW_RELEASE) {
+		_keyPressed[win][key] = false;
+		_keyHolded[win][key] = false;
+	}
+}
+
+void Input::mouse_button_callback(GLFWwindow* win, int button, int action, int mods)
+{
+	if (action == GLFW_PRESS) {
+		if (_mouseButtonPressed[win][button]) {
+			_mouseButtonHolded[win][button] = true;
+		}
+		else {
+			_mouseButtonPressed[win][button] = true;
+		}
+	}
+	else if (action == GLFW_RELEASE) {
+		_mouseButtonPressed[win][button] = false;
+		_mouseButtonHolded[win][button] = false;
+	}
+}
+
+void Input::initForWindow(GLFWwindow* window, bool mainWindow)
+{
+	// Init keys tables
+	_keyPressed[window] = new bool[KEYS_MAX_NUM];
+	_keyHolded[window] = new bool[KEYS_MAX_NUM];
+	glfwSetKeyCallback(window, key_callback);
+	// Init mouse tables
+	_mouseButtonPressed[window] = new bool[KEYS_MAX_NUM];
+	_mouseButtonHolded[window] = new bool[KEYS_MAX_NUM];
+	glfwSetMouseButtonCallback(window, mouse_button_callback);
+
+	// Main Window
+	if (mainWindow || _mainWindow == nullptr) setMainWindow(window);
+}
+
+void Input::freeWindow(GLFWwindow* window)
+{
+	// Delete Keys Tables
+	delete[] _keyPressed[window];
+	delete[] _keyHolded[window];
+	_keyPressed.erase(window);
+	_keyHolded.erase(window);
+
+	// Delete Mouse Buttons Tables
+	delete[] _mouseButtonPressed[window];
+	delete[] _mouseButtonHolded[window];
+	_mouseButtonPressed.erase(window);
+	_mouseButtonHolded.erase(window);
+}
+
+constexpr void Input::setMainWindow(GLFWwindow* window)
+{
+	_mainWindow = window;
+}
+
+constexpr GLFWwindow* Input::getMainWindow()
+{
+	return _mainWindow;
+}
+
+void Input::hideAndLockCursor(GLFWwindow* window)
+{
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+}
+
+void Input::hideCursor(GLFWwindow* window)
+{
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
+}
+
+void Input::keepCursorInWindow(GLFWwindow* window)
+{
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_CAPTURED);
+}
+
+void Input::showCursor(GLFWwindow* window)
+{
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+}
+
+glm::vec2 Input::getMousePos(GLFWwindow* window)
+{
+	double x = 0;
+	double y = 0;
+	glfwGetCursorPos(window, &x, &y);
+	return glm::vec2(x, y);
+}
+
+bool Input::isMouseButtonPressed(GLFWwindow* window, int button)
+{
+	return _mouseButtonPressed[window][button];
+}
+
+bool Input::isMouseButtonReleased(GLFWwindow* window, int button)
+{
+	return !_mouseButtonPressed[window][button];
+}
+
+bool Input::isMouseButtonHold(GLFWwindow* window, int button)
+{
+	return _mouseButtonHolded[window][button];
+}
+
+bool Input::isKeyPressed(GLFWwindow* window, int key)
+{
+	return _keyPressed[window][key];
+}
+
+bool Input::isKeyReleased(GLFWwindow* window, int key)
+{
+	return !_keyPressed[window][key];
+}
+
+bool Input::isKeyHold(GLFWwindow* window, int key)
+{
+	return _keyHolded[window][key];
+}
+
+void Input::hideAndLockCursor()
+{
+	hideAndLockCursor(_mainWindow);
+}
+
+void Input::hideCursor()
+{
+	hideCursor(_mainWindow);
+}
+
+void Input::keepCursorInWindow()
+{
+	keepCursorInWindow(_mainWindow);
+}
+
+void Input::showCursor()
+{
+	showCursor(_mainWindow);
+}
+
+glm::vec2 Input::getMousePos()
+{
+	return getMousePos(_mainWindow);
+}
+
+bool Input::isMouseButtonPressed(int button)
+{
+	return isMouseButtonPressed(_mainWindow, button);
+}
+
+bool Input::isMouseButtonReleased(int button)
+{
+	return isMouseButtonReleased(_mainWindow, button);
+}
+
+bool Input::isMouseButtonHold(int button)
+{
+	return isMouseButtonHold(_mainWindow, button);
+}
+
+bool Input::isKeyPressed(int key)
+{
+	return isKeyPressed(_mainWindow, key);
+}
+
+bool Input::isKeyReleased(int key)
+{
+	return isKeyReleased(_mainWindow, key);
+}
+
+bool Input::isKeyHold(int key)
+{
+	return isKeyHold(_mainWindow, key);
+}
--- src/Input/Input.h
+++ src/Input/Input.h
@@ -5,40 +5,56 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <map>
 
-#define KEYS_MAX_NUM 349;
+#define KEYS_MAX_NUM 349
+#define MOUSE_BUTTONS_MAX_NUM 8
 
 class Input {
 private:
 	static GLFWwindow* _mainWindow;
-	static std::map<GLFWwindow*, bool[]> _pressed;
-	static std::map<GLFWwindow*, bool[]> _holded;
+	
+	static std::map<GLFWwindow*, bool*> _mouseButtonPressed;
+	static std::map<GLFWwindow*, bool*> _mouseButtonHolded;
+
+	static std::map<GLFWwindow*, bool*> _keyPressed;
+	static std::map<GLFWwindow*, bool*> _keyHolded;
 
 	static void key_callback(GLFWwindow* win, int key, int scancode, int action, int mods);
+	static void mouse_button_callback(GLFWwindow* win, int button, int action, int mods);
 public:
-	static void InitForWindow(const GLFWwindow* window, bool mainWindow = false);
-	static void SetMainWindow(const GLFWwindow* window);
-
-	static void 
+	static void initForWindow(GLFWwindow* window, bool mainWindow = false);
+	static void freeWindow(GLFWwindow* window);
+	static constexpr void setMainWindow(GLFWwindow* window);
+	static constexpr GLFWwindow* getMainWindow();
 	
 #pragma region WITHOUT_MAIN_WINDOW
-	static glm::vec2 getMousePos(const GLFWwindow* window) const;
-	static bool getMouseButtonPressed(const GLFWwindow* window, int button) const;
-	static bool getMouseButtonReleased(const GLFWwindow* window, int button) const;
-	static bool getMouseButtonHold(const GLFWwindow* window, int button) const;
+	static void hideAndLockCursor(GLFWwindow* window);
+	static void hideCursor(GLFWwindow* window);
+	static void keepCursorInWindow(GLFWwindow* window);
+	static void showCursor(GLFWwindow* window);
 
-	static bool getKeyPressed(const GLFWwindow* window, int key) const;
-	static bool getKeyReleased(const GLFWwindow* window, int key) const;
-	static bool getKeyHold(const GLFWwindow* window, int key) const;
+	static glm::vec2 getMousePos(GLFWwindow* window);
+	static bool isMouseButtonPressed(GLFWwindow* window, int button);
+	static bool isMouseButtonReleased(GLFWwindow* window, int button);
+	static bool isMouseButtonHold(GLFWwindow* window, int button);
+
+	static bool isKeyPressed(GLFWwindow* window, int key);
+	static bool isKeyReleased(GLFWwindow* window, int key);
+	static bool isKeyHold(GLFWwindow* window, int key);
 #pragma endregion
 
 #pragma region WITH_MAIN_WINDOW
-	static glm::vec2 getMousePos() const;
-	static bool getMouseButtonPressed(int button) const;
-	static bool getMouseButtonReleased(int button) const;
-	static bool getMouseButtonHold(int button) const;
+	static void hideAndLockCursor();
+	static void hideCursor();
+	static void keepCursorInWindow();
+	static void showCursor();
 
-	static bool getKeyPressed(int key) const;
-	static bool getKeyReleased(int key) const;
-	static bool getKeyHold(int key) const;
+	static glm::vec2 getMousePos();
+	static bool isMouseButtonPressed(int button);
+	static bool isMouseButtonReleased(int button);
+	static bool isMouseButtonHold(int button);
+
+	static bool isKeyPressed(int key);
+	static bool isKeyReleased(int key);
+	static bool isKeyHold(int key);
 #pragma endregion
 };

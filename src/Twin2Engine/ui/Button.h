15c840fd8f73437046fd2db8447f30377bb1ff62
MAIPA01 <marcelipatryk@gmail.com>
2024-03-19 16:09:45 +0100

    Event Handler

--- src/Twin2Engine/ui/Button.h
+++ src/Twin2Engine/ui/Button.h
@@ -1,30 +1,29 @@
 #pragma once
 
 #include <core/Component.h>
-#include <functional>
+#include "EventHandler.h"
 
 namespace Twin2Engine {
 	namespace UI {
-		template<typename... Args> using Action = std::function<void(Args)>;
-
 		class Button : public Twin2EngineCore::Component {
 		private:
-			float _width;
-			float _height;
-			bool _interactable;
-
-			std::vector<Action<>> _onClickActions = std::vector<Action<>>();
+			float _width = 0.f;
+			float _height = 0.f;
+			bool _interactable = true;
+			MethodEventHandler _onClickEvent = MethodEventHandler();
 
 		public:
 			void SetWidth(float width);
 			void SetHeight(float height);
 			void SetInteractable(bool interactable);
 
-			void AddOnClickCallback(const Action<>& callback);
-
 			float GetWidth() const;
 			float GetHeight() const;
 			bool IsInteractable() const;
+			const MethodEventHandler& GetOnClickEvent() const;
+
+			void Inizialize() override;
+			void Update() override;
 		};
 	}
 }
--- src/Twin2Engine/ui/EventHandler.h
+++ src/Twin2Engine/ui/EventHandler.h
@@ -4,14 +4,51 @@
 
 namespace Twin2Engine {
 	namespace UI {
-		template<typename V, typename T, typename F> using is_void = typename std::conditional<std::is_same<V, void>::value, T, F>;
+		template<typename T> concept args_t = true;
 
-		template<typename... Args> using Action = typename is_void<Args, std::function<void()>, std::function<void(Args)>>;
-		template<typename Ret, typename... Args> using Func = typename is_void<Ret, Action<Args>, typename is_void<Args, std::function<Ret(void)>, std::function<Ret(Args)>>;
+		template<typename Ret, args_t... Args> using Func = std::function<Ret(Args)>;
+		template<args_t... Args> using Action = Func<void, Args>;
+		using Method = Action<void>;
 
-		//template<typename... Args>
+		template<args_t... Args>
 		class EventHandler {
-			std::vector<Action<void>> _onClickActions = std::vector<Action<void>>();
+		private:
+			std::vector<Action<Args>> _actions = std::vector<Action<Args>>();
+
+		public:
+			EventHandler() = default;
+			virtual ~EventHandler() = default;
+
+			constexpr void AddCallback(const Action<Args>& callback) {
+				_actions.push_back(callback);
+			}
+			void RemoveCallback(const Action<Args>& callback) {
+				for (size_t i = 0; i < _actions.size(); ++i) {
+					if (_actions[i] == callback) {
+						_actions.erase(_actions.begin() + i);
+						break;
+					}
+				}
+			}
+			constexpr void RemoveAllCallbacks() {
+				_actions.clear();
+			}
+
+			void Invoke(Args... args) const {
+				for (Action<Args>& action : _actions) {
+					action(args);
+				}
+			}
+
+			constexpr void operator+=(const Action<Args>& callback) {
+				AddCallback(callback);
+			}
+			constexpr void operator-=(const Action<Args>& callback) {
+				RemoveCallback(callback);
+				return *this;
+			}
 		};
+
+		using MethodEventHandler = EventHandler<void>;
 	}
 }

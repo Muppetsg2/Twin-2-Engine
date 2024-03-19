bcab7e32bdc600e7ed3c33395971c8522b38552b
Mateusz Gapiński <matgap478@gmail.com>
2024-03-19 20:07:44 +0100

    Dodanie globalnych transformacji do Transform wraz z dirtyflagami

--- src/Twin2Engine/Transform.cpp
+++ src/Twin2Engine/Transform.cpp
@@ -1,4 +1,5 @@
 #include <inc/Transform.h>
+#include "Transform.h"
 
 void Twin2EngineCore::Transform::JustAddChild(Transform* child)
 {
@@ -9,6 +10,67 @@ void Twin2EngineCore::Transform::JustRemoveChild(Transform* child)
 {
 	_children.erase(std::find(_children.begin(), _children.end(), child));
 }
+
+
+void Twin2EngineCore::Transform::SetDirtyFlagInChildren()
+{
+	if (!_dirtyFlagInHierarchy)
+	{
+		for (int index = 0; index < _children.size(); index++)
+		{
+			_dirtyFlagInHierarchy = true;
+			_children[index]->SetDirtyFlagInChildren();
+		}
+	}
+}
+
+void Twin2EngineCore::Transform::SetDirtyFlagGlobalPositionInChildren()
+{
+	if (!_dirtyFlagGlobalPosition)
+	{
+		for (int index = 0; index < _children.size(); index++)
+		{
+			_dirtyFlagGlobalPosition = true;
+			_dirtyFlagInHierarchy = true;
+			_children[index]->SetDirtyFlagGlobalPositionInChildren();
+		}
+	}
+}
+
+void Twin2EngineCore::Transform::SetDirtyFlagGlobalRotationInChildren()
+{
+	if (!_dirtyFlagGlobalRotation)
+	{
+		for (int index = 0; index < _children.size(); index++)
+		{
+			_dirtyFlagGlobalRotation = true;
+			_dirtyFlagInHierarchy = true;
+			_children[index]->SetDirtyFlagGlobalRotationInChildren();
+		}
+	}
+}
+void Twin2EngineCore::Transform::SetDirtyFlagGlobalScaleInChildren()
+{
+	if (!_dirtyFlagGlobalScale)
+	{
+		for (int index = 0; index < _children.size(); index++)
+		{
+			_dirtyFlagGlobalScale = true;
+			_dirtyFlagInHierarchy = true;
+			_children[index]->SetDirtyFlagGlobalScaleInChildren();
+		}
+	}
+}
+//void Twin2EngineCore::Transform::ClearDirtyFlagInParents()
+//{
+//	Transform* tranformToClear = this;
+//
+//	do
+//	{
+//		transformToClear->_dirtyFlagInHierarchy = false;
+//		transformToClear = transformToClear->_parent;
+//	} while (transformToClear != nullptr && transformToClear->_dirtyFlagInHierarchy);
+//}
 
 Twin2EngineCore::Transform::Transform()
 {
@@ -22,9 +84,8 @@ Twin2EngineCore::Transform::Transform()
 
 	_parent = nullptr;
 
-	_transformMatrix = glm::mat4(1.0f);
+	_globalTransformMatrix = glm::mat4(1.0f);
 
-	dirtyFlag = false;
 }
 
 void Twin2EngineCore::Transform::SetParent(Transform* parent)
@@ -40,7 +101,11 @@ void Twin2EngineCore::Transform::SetParent(Transform* parent)
 	}
 
 	_parent = parent;
-	dirtyFlag = true;
+
+	//_dirtyFlag = true;
+	_dirtyFlagInHierarchy = true;
+	
+	SetDirtyFlagInChildren();
 }
 
 Twin2EngineCore::Transform* Twin2EngineCore::Transform::GetParent() const
@@ -73,87 +138,287 @@ size_t Twin2EngineCore::Transform::GetChildCount() const
 	return _children.size();
 }
 
+#pragma region POSITIONS
+
 void Twin2EngineCore::Transform::SetLocalPosition(const glm::vec3& localPosition)
 {
 	_localPosition = localPosition;
-	dirtyFlag = true;
+
+	_dirtyFlag = true;
+	_dirtyFlagGlobalPosition = true;
+	_dirtyFlagLocalPosition = false;
 }
+
 
 glm::vec3 Twin2EngineCore::Transform::GetLocalPosition() const
 {
+	RecalculateLocalPosition();
+
 	return _localPosition;
 }
 
-void Twin2EngineCore::Transform::SetLocalRotation(const glm::vec3& localRotation)
+void Twin2EngineCore::Transform::RecalculateLocalPosition()
 {
-	_localRotation = localRotation;
-	dirtyFlag = true;
-}
+	if (_dirtyFlagLocalPosition)
+	{
+		if (_parent == nullptr)
+		{
+			_localPosition = _globalPosition;
+		}
+		else
+		{
+			_localPosition = glm::inverse(_parent->GetTransformMatrix()) * _globalPosition;
+		}
 
-glm::vec3 Twin2EngineCore::Transform::GetLocalRotation() const
-{
-	return _localRotation;
-}
-
-void Twin2EngineCore::Transform::SetLocalScale(const glm::vec3& localScale)
-{
-	_localScale = localScale;
-	dirtyFlag = true;
-}
-
-glm::vec3 Twin2EngineCore::Transform::GetLocalScale() const
-{
-	return _localScale;
+		_dirtyFlagLocalPosition = false;
+	}
 }
 
 void Twin2EngineCore::Transform::SetGlobalPosition(const glm::vec3& globalPosition)
 {
+	_globalPosition = globalPosition;
+
+	_dirtyFlag = true;
+	_dirtyFlagLocalPosition = true;
+	_dirtyFlagGlobalPosition = false;
 }
 
-glm::vec3 Twin2EngineCore::Transform::GetGlobalPosition() const
+glm::vec3 Twin2EngineCore::Transform::GetGlobalPosition()
 {
+	RecalculateGlobalPosition();
+
 	return _globalPosition;
+}
+
+void Twin2EngineCore::Transform::RecalculateGlobalPosition()
+{
+	if (_dirtyFlagGlobalPosition)
+	{
+		RecalculateTransformMatrix();
+
+		_globalPosition = glm::vec3(_globalTransformMatrix[3]);
+
+		_dirtyFlagGlobalPosition = false;
+	}
+}
+
+#pragma endregion
+
+#pragma region ROTATIONS
+
+void Twin2EngineCore::Transform::SetLocalRotation(const glm::vec3& localRotation)
+{
+	_localRotation = localRotation;
+	_localRotationQuat = glm::quat(localRotation);
+
+	_dirtyFlag = true;
+	_dirtyFlagGlobalRotation = true;
+	_dirtyFlagLocalRotation = false;
+
+	void SetDirtyFlagGlobalRotationInChildren();
+}
+
+glm::vec3 Twin2EngineCore::Transform::GetLocalRotation()
+{
+	RecalculateLocalRotation();
+
+	return _localRotation;
+}
+
+void Twin2EngineCore::Transform::SetLocalRotation(const glm::quat& localRotation)
+{
+	_localRotationQuat = localRotation;
+	_localRotationt = glm::eulerAngles(localRotation);
+
+	_dirtyFlag = true;
+	_dirtyFlagGlobalRotation = true;
+	_dirtyFlagLocalRotation = false;
+}
+glm::quat Twin2EngineCore::Transform::GetLocalRotationQuat()
+{
+	RecalculateLocalRotation();
+
+	return _localRotationQuat;
+}
+
+void Twin2EngineCore::Transform::RecalculateLocalRotation()
+{
+	if (_dirtyFlagLocalRotation)
+	{
+		if (_parent == nullptr)
+		{
+			_localRotationQuat = _globalRotationQuat;
+			_localRotation = _globalRotation;
+		}
+		else
+		{
+			_localRotationQuat = glm::inverse(_parent->GetGlobalRotationQuat()) * _globalRotationQuat;
+			_localRotation = glm::eulerAngles(_localRotationQuat);
+		}
+
+		_dirtyFlagLocalRotation = false;
+	}
 }
 
 void Twin2EngineCore::Transform::SetGlobalRotation(const glm::vec3& globalRotation)
 {
+	_globalRotation = globalRotation;
+	_globalRotationQuat = glm::quat(globalRotation);
+
+	_dirtyFlag = true;
+	_dirtyFlagLocalRotation = true;
+	_dirtyFlagGlobalRotation = false;
 }
 
-glm::vec3 Twin2EngineCore::Transform::GetGlobalRotation() const
+glm::vec3 Twin2EngineCore::Transform::GetGlobalRotation()
 {
+	RecalculateGlobalRotation();
+
+	if (_dirtyFlagGlobalRotationQuat2Euler)
+	{
+		_globalRotation = glm::eulerAngles(_globalRotationQuat);
+	}
+
 	return _globalRotation;
 }
 
-void Twin2EngineCore::Transform::SetGlobalScale(const glm::vec3& globalScale)
+glm::quat Twin2EngineCore::Transform::GetGlobalRotationQuat()
 {
+	RecalculateGlobalRotation();
+
+	return _globalRotationQuat;
 }
 
-glm::vec3 Twin2EngineCore::Transform::GetGlobalScale() const
+void Twin2EngineCore::Transform::RecalculateGlobalRotation()
 {
+	if (_dirtyFlagGlobalRotation)
+	{
+		if (_parent == nullptr)
+		{
+			_globalRotationQuat = _localRotationQuat;
+		}
+		else
+		{
+			_parent->RecalculateGlobalRotation();
+
+			_globalRotationQuat = _parent->_globalRotation * _localRotationQuat;
+		}
+
+		_dirtyFlagGlobalRotation = false;
+		_dirtyFlagGlobalRotationQuat2Euler = true;
+	}
+}
+
+#pragma endregion
+
+#pragma region SCALES
+
+void Twin2EngineCore::Transform::SetLocalScale(const glm::vec3& localScale)
+{
+	_localScale = localScale;
+	_dirtyFlag = true;
+	_dirtyFlagGlobalScale = true;
+	_dirtyFlagLocalScale = false;
+
+	SetDirtyFlagGlobalScaleInChildren();
+}
+
+glm::vec3 Twin2EngineCore::Transform::GetLocalScale() const
+{
+	RecalculateLocalScale();
+
+	return _localScale;
+}
+
+void Twin2EngineCore::Transform::RecalculateLocalScale()
+{
+	if (_dirtyFlagLocalScale)
+	{
+		if (_parent == nullptr)
+		{
+			_localScale = _globalScale;
+		}
+		else
+		{
+			_localScale = _globalScale / _parent->GetGlobalScale();
+		}
+
+		_dirtyFlagLocalScale = false;
+	}
+}
+
+
+void Twin2EngineCore::Transform::SetGlobalScale(const glm::vec3& globalScale)
+{
+	_globalScale = globalScale;
+	_dirtyFlag = true;
+	_dirtyFlagLocalScale = true;
+	_dirtyFlagGlobalScale = false;
+}
+
+glm::vec3 Twin2EngineCore::Transform::GetGlobalScale()
+{
+	RecalculateGlobalScale();
+	//RecalculateTransformMatrix();
+	//_globalScale = glm::vec3(_globalTransformMatrix[0][0], _globalTransformMatrix[1][1], _globalTransformMatrix[2][2]);
+
 	return _globalScale;
 }
 
-glm::mat4 Twin2EngineCore::Transform::GetTransformMatrix()
+void Twin2EngineCore::Transform::RecalculateGlobalScale()
 {
-	//if (dirtyFlag)
+	if (_dirtyFlagGlobalScale)
 	{
-		dirtyFlag = false;
-
-		_transformMatrix = glm::mat4(1.0f);
-
-		_transformMatrix = glm::translate(_transformMatrix, _localPosition);
-		_transformMatrix = glm::rotate(_transformMatrix, _localRotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
-		_transformMatrix = glm::rotate(_transformMatrix, _localRotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
-		_transformMatrix = glm::rotate(_transformMatrix, _localRotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
-		_transformMatrix = glm::scale(_transformMatrix, _localScale);
-
-		if (_parent)
+		if (_parent == nullptr)
 		{
-			//parent->RecalculateTransform();
-			_transformMatrix = _parent->_transformMatrix * _transformMatrix;
+			_globalScale = _localScale;
+		}
+		else
+		{
+			_globalScale = _parent->GetGlobalScale() * _localScale;
 		}
 
+		_dirtyFlagGlobalScale = false;
+	}
+}
+
+
+#pragma endregion
+
+glm::mat4 Twin2EngineCore::Transform::GetTransformMatrix()
+{
+	RecalculateTransformMatrix();
+
+	return _globalTransformMatrix;
+}
+
+void Twin2EngineCore::Transform::RecalculateTransformMatrix()
+{
+	if (_dirtyFlag)
+	{
+		_localTransformMatrix = glm::mat4(1.0f);
+
+		_localTransformMatrix = glm::translate(_localTransformMatrix, GetLocalPosition());
+
+		glm::vec3 rotation = GetLocalRotation();
+
+		_localTransformMatrix = glm::rotate(_localTransformMatrix, rotation.x, glm::vec3(1.0f, 0.0f, 0.0f));
+		_localTransformMatrix = glm::rotate(_localTransformMatrix, rotation.y, glm::vec3(0.0f, 1.0f, 0.0f));
+		_localTransformMatrix = glm::rotate(_localTransformMatrix, rotation.z, glm::vec3(0.0f, 0.0f, 1.0f));
+		_localTransformMatrix = glm::scale(_localTransformMatrix, GetLocalScale());
+
+		_dirtyFlag = false;
 	}
 
-	return _transformMatrix;
+
+	if (_dirtyFlagInHierarchy)
+	{
+		if (_parent != nullptr)
+		{
+			//_parent->RecalculateTransformMatrix();
+			_globalTransformMatrix = _parent->GetTransformMatrix() * _localTransformMatrix;
+		}
+
+		_dirtyFlagInHierarchy = false;
+	}
 }
--- src/Twin2Engine/inc/Transform.h
+++ src/Twin2Engine/inc/Transform.h
@@ -5,6 +5,7 @@
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 
 #include <vector>
@@ -18,16 +19,32 @@ namespace Twin2EngineCore
 	private:
 		glm::vec3 _localPosition;
 		glm::vec3 _localRotation;
+		glm::quat _localRotationQuat;
 		glm::vec3 _localScale;
 
 
 		glm::vec3 _globalPosition;
 		glm::vec3 _globalRotation;
+		glm::quat _globalRotationQuat;
 		glm::vec3 _globalScale;
 
-		bool dirtyFlag;
+		bool _dirtyFlag = false;
+		bool _dirtyFlagInHierarchy = false;
 
-		glm::mat4 _transformMatrix;
+		bool _dirtyFlagGlobalPosition = false;
+		bool _dirtyFlagLocalPosition = false;
+
+		bool _dirtyFlagGlobalRotation = false;
+		bool _dirtyFlagLocalRotation = false;
+
+		bool _dirtyFlagGlobalScale = false;
+		bool _dirtyFlagLocalScale = false;
+
+		//bool _dirtyFlagLocalRotationQuat2Euler;
+		bool _dirtyFlagGlobalRotationQuat2Euler = false;
+
+		glm::mat4 _localTransformMatrix;
+		glm::mat4 _globalTransformMatrix;
 
 		Transform* _parent;
 
@@ -36,6 +53,23 @@ namespace Twin2EngineCore
 
 		void JustAddChild(Transform* child);
 		void JustRemoveChild(Transform* child);
+
+		void SetDirtyFlagInChildren();
+		//void ClearDirtyFlagInParents();
+		void SetDirtyFlagGlobalPositionInChildren();
+		void SetDirtyFlagGlobalRotationInChildren();
+		void SetDirtyFlagGlobalScaleInChildren();
+
+		void RecalculateTransformMatrix();
+
+		void RecalculateGlobalPosition();
+		void RecalculateLocalPosition();
+
+		void RecalculateGlobalRotation();
+		void RecalculateLocalRotation();
+
+		void RecalculateGlobalScale();
+		void RecalculateLocalScale();
 
 	public:
 		Transform();
@@ -56,16 +90,19 @@ namespace Twin2EngineCore
 		void SetLocalPosition(const glm::vec3& localPosition);
 		glm::vec3 GetLocalPosition() const;
 		void SetLocalRotation(const glm::vec3& localRotation);
-		glm::vec3 GetLocalRotation() const;
+		glm::vec3 GetLocalRotation();
+		void SetLocalRotation(const glm::quat& localRotation);
+		glm::quat GetLocalRotationQuat();
 		void SetLocalScale(const glm::vec3& localScale);
 		glm::vec3 GetLocalScale() const;
 
 		void SetGlobalPosition(const glm::vec3& globalPosition);
-		glm::vec3 GetGlobalPosition() const;
+		glm::vec3 GetGlobalPosition();
 		void SetGlobalRotation(const glm::vec3& globalRotation);
-		glm::vec3 GetGlobalRotation() const;
+		glm::vec3 GetGlobalRotation();
+		glm::quat GetGlobalRotationQuat();
 		void SetGlobalScale(const glm::vec3& globalScale);
-		glm::vec3 GetGlobalScale() const;
+		glm::vec3 GetGlobalScale();
 
 		glm::mat4 GetTransformMatrix();
 	};

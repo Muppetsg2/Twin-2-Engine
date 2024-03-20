c6aa7a6df8bb3a22110e65fd64243e7f62fe9404
MAIPA01 <47190521+MAIPA01@users.noreply.github.com>
2024-03-20 12:29:53 +0100

    idea

--- src/Twin2Engine/core/TypesMap.h
+++ src/Twin2Engine/core/TypesMap.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <vector>
+#include <string>
+
+template<typename... Types>
+class TypesMap {
+private:
+	std::vector<std::string> _names;
+
+public:
+	template<std::enable_if_t<sizeof...(Types...) > 0, bool> = true>
+	TypesMap(const std::vector<std::string>& names) {
+
+	}
+	template<false> TypesMap() = default;
+	virtual ~TypesMap() = default;
+
+	template<typename T, typename... R> TypesMap<R...> AddType(const std::string& name) {
+		if (_names.find(name) != _names.end()) {
+			std::vector<std::string> names = _names;
+			name.push_back(name);
+			return TypesMap<Types..., T>(names);
+		}
+		return TypesMap<Types...>();
+	}
+};

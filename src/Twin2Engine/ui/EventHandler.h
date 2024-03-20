e03b0411d251a3be84807c860b8af14dd7518135
MAIPA01 <marcelipatryk@gmail.com>
2024-03-20 01:47:33 +0100

    Update EventHandler.h

--- src/Twin2Engine/ui/EventHandler.h
+++ src/Twin2Engine/ui/EventHandler.h
@@ -4,26 +4,23 @@
 
 namespace Twin2Engine {
 	namespace UI {
-		template<typename Ty> using is_void = std::is_same<Ty, void>;
-		template<typename Ty, typename T, typename F> using if_void = std::conditional<is_void<Ty>::value, T, F>::type;
-
-		template<typename... Args> using Action = if_void<Args..., std::function<void(void)>, std::function<void(Args...)>>;
-		using Method = Action<void>;
-		template<typename Ret, typename... Args> using Func = if_void<Args..., std::function<Ret(void)>, if_void<Ret, Action<Args>, std::function<Ret(Args...)>>>;
+		template<typename Ret, typename... Args> using Func = std::function<Ret(Args...)>;
+		template<typename... Args> using Action = Func<void, Args...>;
+		using Method = Action<>;
 
 		template<typename... Args>
 		class EventHandler {
 		private:
-			std::vector<Action<Args>> _actions = std::vector<Action<Args>>();
+			std::vector<Action<Args...>> _actions = std::vector<Action<Args...>>();
 
 		public:
 			EventHandler() = default;
 			virtual ~EventHandler() = default;
 
-			constexpr void AddCallback(const Action<Args>& callback) {
+			constexpr void AddCallback(const Action<Args...>& callback) {
 				_actions.push_back(callback);
 			}
-			void RemoveCallback(const Action<Args>& callback) {
+			void RemoveCallback(const Action<Args...>& callback) {
 				for (size_t i = 0; i < _actions.size(); ++i) {
 					if (_actions[i] == callback) {
 						_actions.erase(_actions.begin() + i);
@@ -35,21 +32,20 @@ namespace Twin2Engine {
 				_actions.clear();
 			}
 
-
 			void Invoke(Args... args) const {
-				for (Action<Args>& action : _actions) {
+				for (Action<Args...>& action : _actions) {
 					action(args);
 				}
 			}
 
-			constexpr void operator+=(const Action<Args>& callback) {
+			constexpr void operator+=(const Action<Args...>& callback) {
 				AddCallback(callback);
 			}
-			constexpr void operator-=(const Action<Args>& callback) {
+			constexpr void operator-=(const Action<Args...>& callback) {
 				RemoveCallback(callback);
 				return *this;
 			}
 		};
-		using MethodEventHandler = EventHandler<void>;
+		using MethodEventHandler = EventHandler<>;
 	}
 }

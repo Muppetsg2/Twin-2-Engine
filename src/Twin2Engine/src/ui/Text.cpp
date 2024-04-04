9b64cf5c299a78453661611038ad17188b35a23d
MAIPA01 <47190521+MAIPA01@users.noreply.github.com>
2024-04-04 16:42:11 +0200

    Text Component Fixes

--- src/Twin2Engine/src/ui/Text.cpp
+++ src/Twin2Engine/src/ui/Text.cpp
@@ -56,21 +56,26 @@ void Text::SetColor(const vec4& color)
 void Text::SetText(const string& text)
 {
 	if (_text != text) {
-		// Zastêpowanie
-		for (size_t i = 0; i < _text.size() && i < text.size(); ++i) {
-			if (_text[i] != text[i]) {
-				_textCache[i] = _font->GetCharacter(text[i], _size);
+		if (_size != 0 && _font != nullptr) {
+			// Zastêpowanie
+			for (size_t i = 0; i < _text.size() && i < text.size(); ++i) {
+				if (_text[i] != text[i]) {
+					_textCache[i] = _font->GetCharacter(text[i], _size);
+				}
+			}
+
+			// Dodawanie kolejnych
+			if (_text.size() < text.size()) {
+				vector<Character*> chars = _font->GetText(text.substr(_text.size(), text.size() - _text.size()), _size);
+				for (auto& c : chars) _textCache.push_back(c);
+			}
+			// Odejmowanie nadmiaru
+			else if (_text.size() > text.size()) {
+				for (size_t i = 0; i < _text.size() - text.size(); ++i) _textCache.erase(_textCache.begin() + text.size() + i);
 			}
 		}
-
-		// Dodawanie kolejnych
-		if (_textCache.size() < text.size()) {
-			vector<Character*> chars = _font->GetText(text.substr(_text.size() - 1, text.size() - _text.size() - 1), _size);
-			for (auto& c : chars) _textCache.push_back(c);
-		}
-		// Odejmowanie nadmiaru
-		else if (_textCache.size() > text.size()) {
-			for (size_t i = 0; i < _text.size() - text.size(); ++i) _textCache.erase(_textCache.begin() + text.size() + i);
+		else {
+			_textCache.clear();
 		}
 		_text = text;
 	}
@@ -81,7 +86,9 @@ void Text::SetSize(uint32_t size)
 	if (_size != size) {
 		_size = size;
 		_textCache.clear();
-		_textCache = _font->GetText(_text, size);
+		if (_font != nullptr) {
+			_textCache = _font->GetText(_text, size);
+		}
 	}
 }
 
@@ -95,7 +102,9 @@ void Text::SetFont(Font* font)
 	if (_font != font) {
 		_font = font;
 		_textCache.clear();
-		_textCache = _font->GetText(_text, _size);
+		if (_size != 0) {
+			_textCache = _font->GetText(_text, _size);
+		}
 	}
 }
 

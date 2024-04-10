1e8d7506400ea8cb5ad760d537d3c59aaf2dcb19
Mateusz Gapiński <matgap478@gmail.com>
2024-04-10 20:38:13 +0200

    Dodanie do Twin2Engine::Core klasy Random oraz utworzenie CMakeLists dla Generation w Additional

---
+++ src/Additional/Generation/pch.h
@@ -0,0 +1,22 @@
+#pragma once
+
+// GLM
+#include <glm/glm.hpp>
+
+
+// YAML
+//#include <yaml-cpp/yaml.h>
+
+// SPDLOG
+#include <spdlog/spdlog.h>
+
+// STD
+//#include <queue>
+//#include <vector>
+
+// TILEMAP
+#include <Tilemap/HexagonalTile.h>
+#include <Tilemap/HexagonalTilemap.h>
+
+// TWIN2ENGINE::CORE
+#include <core/Random.h>

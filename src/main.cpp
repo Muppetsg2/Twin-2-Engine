741936917cae22fc563defdcd0dfeeb5286068c6
MAIPA01 <marcelipatryk@gmail.com>
2024-06-27 17:39:32 +0200

    Enable Fullscreen

--- src/main.cpp
+++ src/main.cpp
@@ -167,7 +167,7 @@ void end_imgui();
 constexpr const char* WINDOW_NAME = "Echoes Of Fame";
 constexpr int32_t WINDOW_WIDTH  = 1920;
 constexpr int32_t WINDOW_HEIGHT = 1080;
-constexpr bool WINDOW_FULLSCREEN = false;
+constexpr bool WINDOW_FULLSCREEN = true;
 
 #if DISPLAY_SPLASH_SCREEN
 constexpr const char* SPLASH_SCREEN_TEXTURE = "res/textures/splashScreen2.png";
